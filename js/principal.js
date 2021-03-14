d0ba886bbbf1c03990c1a29c036cd85753e85b48
Fulano <detias@outlook.com>
2021-03-14 01:21:44 -0300

    02 - Variaveis e operadores

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,33 @@
 let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida - Nutricionista";
 console.log(titulo);
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+let tdImc = paciente.querySelector(".info-imc");
+
+if(peso < 0 || peso > 1000){
+    console.log("Peso Inválido.");
+        tdImc.textContent = "Peso Inválido.";
+
+}
+else if(altura < 0 || altura > 3.00){
+    console.log("Altura Inválida.");
+    tdImc.textContent = "Peso Inválido.";
+}
+else{
+    var imc = peso / (altura * 2);
+    tdImc.textContent = imc;
+}
+
+
+
+console.log(paciente); // TR
+console.log(tdPeso);// TD = que tem o peso
+
+console.log(tdImc.textContent);
