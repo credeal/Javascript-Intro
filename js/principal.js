b0e853453516fe260f00b2b4de915ceaa1b65176
Fulano <detias@outlook.com>
2021-03-16 19:11:52 -0300

    Arrays, Loop e Estilos

--- js/principal.js
+++ js/principal.js
@@ -2,32 +2,23 @@ let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida - Nutricionista";
 console.log(titulo);
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes)
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
 
-let tdImc = paciente.querySelector(".info-imc");
+for (let i = 0; i < pacientes.length; i++) {
+    var peso = pacientes[i].querySelector(".info-peso").textContent;
+    var altura = pacientes[i].querySelector(".info-altura").textContent;
+    var imc = pacientes[i].querySelector(".info-imc");
 
-if(peso < 0 || peso > 1000){
-    console.log("Peso Inválido.");
-        tdImc.textContent = "Peso Inválido.";
-
+    if(peso < 0 || peso > 1000){
+        pacientes[i].classList.add("cor-de-erro");
+        imc.textContent = "Peso Inválido.";
+    }
+    else if(altura < 0 || altura > 3.00){
+        pacientes[i].classList.add("cor-de-erro"); //Boa pratica é adicionar uma classe para mudar o visual
+        imc.textContent = "Altura Invalída.";
+    }
+    else
+        imc.textContent =((peso / (altura * altura))).toFixed(2);
 }
-else if(altura < 0 || altura > 3.00){
-    console.log("Altura Inválida.");
-    tdImc.textContent = "Peso Inválido.";
-}
-else{
-    var imc = peso / (altura * 2);
-    tdImc.textContent = imc;
-}
-
-
-
-console.log(paciente); // TR
-console.log(tdPeso);// TD = que tem o peso
-
-console.log(tdImc.textContent);
